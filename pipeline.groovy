1d438e804a05a042a0c4523bac298895e0c0d22e
Aaman Tamboli <amantamboli671@gmail.com>
2025-02-14 23:00:55 +0530

    Add files via upload

---
+++ pipeline.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any 
+    stages{
+        stage('Pull'){
+            steps{
+                git branch: 'main', url: 'https://github.com/mayurmwagh/CDEC-B21-Frontend.git'
+            }
+        }
+        stage('Build'){
+            steps{
+                sh '''
+                    npm install 
+                    ng build
+                '''
+            }
+        }
+        stage(''){
+            steps{
+                sh '''
+                    aws s3 cp --recursive dist/angular-frontend/ s3://cdec21-frontend-thcbz-frontend-project-bux/
+                '''
+            }
+        }
+    }
+}
